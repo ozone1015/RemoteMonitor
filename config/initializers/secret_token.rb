09a342bc7edb2b682fe4593cf0f2645f9fafb352
Chase McCarthy <ozone1015@gmail.com>
2011-02-20 01:56:20 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RemoteMonitor::Application.config.secret_token = '073eb4c4bce871166465e633b5f06f166240cf19571eb4d0c9c01da50660883efa40e40ae5ec18958315d17e21828d8c48e91b5b3932526778c9c142d3ad9931'
